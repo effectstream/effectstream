9081a2b2a06ff971db466120e7523bec11e57a6e
Eddie <edward@dcspark.io>
2025-07-15 22:59:07 -0400

    rm files

--- packages/chains/evm/contracts/plugin/mod.ts
+++
@@ -1,6 +0,0 @@
-// TODO: all this has to be updated to Hardhat v3
-
-// import "./paimaL2.js";
-// import "./deployment.js";
-// export * from "./recommendedHardhat.js";
-// export * from "./deployment.js";
