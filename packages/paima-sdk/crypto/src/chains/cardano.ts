39964ef95f2ea254f87f53649e25ef912363d5ae
Edward Alvarado <ac.edward@gmail.com>
2025-07-17 14:23:04 -0400

    Update packages/paima-sdk/crypto/src/chains/cardano.ts
    Co-authored-by: Sebastien Guillemot <SebastienGllmt@gmail.com>

--- packages/paima-sdk/crypto/src/chains/cardano.ts
+++ packages/paima-sdk/crypto/src/chains/cardano.ts
@@ -1,28 +1,28 @@
-// import { doLog } from '@paima/utils';
-// import type { IVerify } from './IVerify.js';
+import { CardanoAddress, doLog, Signature, TypeboxHelpers } from '@paima/utils';
+import type { IVerify } from './IVerify.js';
+import { Value } from '@sinclair/typebox/value';
 
-// export class CardanoCrypto implements IVerify {
-//   verifyAddress = async (address: string): Promise<boolean> => {
-//     // TODO: improve
-//     return await Promise.resolve(/^addr1[a-zA-Z0-9]+$/.test(address));
-//   };
-//   verifySignature = async (
-//     userAddress: string,
-//     message: string,
-//     sigStruct: string
-//   ): Promise<boolean> => {
-//     try {
-//       const [signature, key, ...remainder] = sigStruct.split('+');
-//       if (!signature || !key || remainder.length > 0) {
-//         return false;
-//       }
-//       const { default: verifyCardanoDataSignature } = await import(
-//         '@cardano-foundation/cardano-verify-datasignature'
-//       );
-//       return verifyCardanoDataSignature(signature, key, message, userAddress);
-//     } catch (err) {
-//       doLog('[address-validator] error verifying cardano signature:', err);
-//       return false;
-//     }
-//   };
-// }
+export class CardanoCrypto implements IVerify {
+  verifyAddress = (address: string): address is CardanoAddress => {
+    return Value.Check(TypeboxHelpers.Cardano.Address, address);
+  };
+  verifySignature = async (
+    userAddress: CardanoAddress,
+    message: string,
+    sigStruct: Signature
+  ): Promise<boolean> => {
+    try {
+      const [signature, key, ...remainder] = sigStruct.split('+');
+      if (!signature || !key || remainder.length > 0) {
+        return false;
+      }
+      const { default: verifyCardanoDataSignature } = await import(
+        '@cardano-foundation/cardano-verify-datasignature'
+      );
+      return verifyCardanoDataSignature.default(signature, key, message, userAddress);
+    } catch (err) {
+      doLog('[address-validator] error verifying cardano signature:', err);
+      return false;
+    }
+  };
+}
