1125b5ea918e525edc3c4602073544f34fab67e3
Edward Alvarado <ac.edward@gmail.com>
2025-07-17 14:22:51 -0400

    Update packages/paima-sdk/crypto/src/chains/polkadot.ts
    Co-authored-by: Sebastien Guillemot <SebastienGllmt@gmail.com>

--- packages/paima-sdk/crypto/src/chains/polkadot.ts
+++ packages/paima-sdk/crypto/src/chains/polkadot.ts
@@ -1,28 +1,28 @@
-// import { doLog } from '@paima/utils';
-// import type { IVerify } from './IVerify.js';
+import { doLog, Signature, SubstrateAddress, TypeboxHelpers } from '@paima/utils';
+import type { IVerify } from './IVerify.js';
+import { Value } from '@sinclair/typebox/value';
 
-// export class PolkadotCrypto implements IVerify {
-//   verifyAddress = async (address: string): Promise<boolean> => {
-//     // TODO: improve
-//     return await Promise.resolve(/^[1-9A-HJ-NP-Za-km-z]{47,48}$/.test(address));
-//   };
-//   verifySignature = async (
-//     userAddress: string,
-//     message: string,
-//     signature: string
-//   ): Promise<boolean> => {
-//     try {
-//       const { cryptoWaitReady, decodeAddress, signatureVerify } = await import(
-//         '@polkadot/util-crypto'
-//       );
-//       const { u8aToHex } = await import('@polkadot/util');
-//       await cryptoWaitReady();
-//       const publicKey = decodeAddress(userAddress);
-//       const hexPublicKey = u8aToHex(publicKey);
-//       return signatureVerify(message, signature, hexPublicKey).isValid;
-//     } catch (err) {
-//       doLog('[address-validator] error verifying polkadot signature:', err);
-//       return false;
-//     }
-//   };
-// }
+export class PolkadotCrypto implements IVerify {
+  verifyAddress = (address: string): address is SubstrateAddress => {
+    return Value.Check(TypeboxHelpers.Substrate.Address, address);
+  };
+  verifySignature = async (
+    userAddress: SubstrateAddress,
+    message: string,
+    signature: Signature
+  ): Promise<boolean> => {
+    try {
+      const { cryptoWaitReady, decodeAddress, signatureVerify } = await import(
+        '@polkadot/util-crypto'
+      );
+      const { u8aToHex } = await import('@polkadot/util');
+      await cryptoWaitReady();
+      const publicKey = decodeAddress(userAddress);
+      const hexPublicKey = u8aToHex(publicKey);
+      return signatureVerify(message, signature, hexPublicKey).isValid;
+    } catch (err) {
+      doLog('[address-validator] error verifying polkadot signature:', err);
+      return false;
+    }
+  };
+}
