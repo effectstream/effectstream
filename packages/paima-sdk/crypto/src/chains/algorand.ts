e08a860272a569aa8b34be7aa1cd2e2157062995
Edward Alvarado <ac.edward@gmail.com>
2025-07-17 14:23:10 -0400

    Update packages/paima-sdk/crypto/src/chains/algorand.ts
    Co-authored-by: Sebastien Guillemot <SebastienGllmt@gmail.com>

--- packages/paima-sdk/crypto/src/chains/algorand.ts
+++ packages/paima-sdk/crypto/src/chains/algorand.ts
@@ -1,72 +1,28 @@
-// import type { SignedTransaction, Transaction, SuggestedParams } from 'algosdk';
-// import { doLog, hexStringToUint8Array } from '@paima/utils';
-// import web3UtilsPkg from 'web3-utils';
-// import type { IVerify } from './IVerify.js';
+import { CardanoAddress, doLog, Signature, TypeboxHelpers } from '@paima/utils';
+import type { IVerify } from './IVerify.js';
+import { Value } from '@sinclair/typebox/value';
 
-// export class AlgorandCrypto implements IVerify {
-//   verifyAddress = async (address: string): Promise<boolean> => {
-//     // TODO: improve
-//     return await Promise.resolve(/^[A-Z2-7]{58}$/.test(address));
-//   };
-//   verifySignature = async (
-//     userAddress: string,
-//     message: string,
-//     signature: string
-//   ): Promise<boolean> => {
-//     try {
-//       const sig = Buffer.from(hexStringToUint8Array(signature));
-//       const txn = await this.buildAlgorandTransaction(userAddress, message);
-//       const signedTx: SignedTransaction = {
-//         txn,
-//         sig,
-//       };
-//       return await this.verifySignedTransaction(signedTx);
-//     } catch (err) {
-//       doLog(`[funnel] error verifying algorand signature: ${err}`);
-//       return await Promise.resolve(false);
-//     }
-//   };
-
-//   buildAlgorandTransaction = async (userAddress: string, message: string): Promise<Transaction> => {
-//     const hexMessage = web3UtilsPkg.utf8ToHex(message).slice(2);
-//     const msgArray = hexStringToUint8Array(hexMessage);
-//     const SUGGESTED_PARAMS: SuggestedParams = {
-//       fee: 0,
-//       firstRound: 10,
-//       lastRound: 10,
-//       genesisID: 'mainnet-v1.0',
-//       genesisHash: 'wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=',
-//     };
-//     const { makePaymentTxnWithSuggestedParams } = await import('algosdk');
-//     return makePaymentTxnWithSuggestedParams(
-//       userAddress,
-//       userAddress,
-//       0,
-//       undefined,
-//       msgArray,
-//       SUGGESTED_PARAMS
-//     );
-//   };
-
-//   verifySignedTransaction = async (signedTransaction: SignedTransaction): Promise<boolean> => {
-//     if (signedTransaction.sig === undefined) return false;
-
-//     const pkBytes = signedTransaction.txn.from.publicKey;
-
-//     const signatureBytes = new Uint8Array(signedTransaction.sig);
-
-//     const { encodeObj } = await import('algosdk');
-//     const transactionBytes = encodeObj(signedTransaction.txn.get_obj_for_encoding());
-//     const messageBytes = new Uint8Array(transactionBytes.length + 2);
-//     messageBytes.set(Buffer.from('TX'));
-//     messageBytes.set(transactionBytes, 2);
-
-//     const { sign } = await import('tweetnacl');
-//     return sign.detached.verify(messageBytes, signatureBytes, pkBytes);
-//   };
-
-//   decodeSignedTransaction = async (signedTx: Uint8Array): Promise<SignedTransaction> => {
-//     const { decodeSignedTransaction } = await import('algosdk');
-//     return decodeSignedTransaction(signedTx);
-//   };
-// }
+export class CardanoCrypto implements IVerify {
+  verifyAddress = (address: string): address is CardanoAddress => {
+    return Value.Check(TypeboxHelpers.Cardano.Address, address);
+  };
+  verifySignature = async (
+    userAddress: CardanoAddress,
+    message: string,
+    sigStruct: Signature
+  ): Promise<boolean> => {
+    try {
+      const [signature, key, ...remainder] = sigStruct.split('+');
+      if (!signature || !key || remainder.length > 0) {
+        return false;
+      }
+      const { default: verifyCardanoDataSignature } = await import(
+        '@cardano-foundation/cardano-verify-datasignature'
+      );
+      return verifyCardanoDataSignature.default(signature, key, message, userAddress);
+    } catch (err) {
+      doLog('[address-validator] error verifying cardano signature:', err);
+      return false;
+    }
+  };
+}
