4418601eedf7d43cf0c687e99bb5ea52513534c0
Eduardo Soto <esotom92@gmail.com>
2025-07-30 01:28:00 -0400

    contract compile script

--- example/contracts/midnight/deploy.ts
+++ example/contracts/midnight/deploy.ts
@@ -77,7 +77,7 @@ class StandaloneConfig {
   );
   indexer = "http://127.0.0.1:8088/api/v1/graphql";
   indexerWS = "ws://127.0.0.1:8088/api/v1/graphql/ws";
-  node = "http://127.0.0.1:9955";
+  node = "http://127.0.0.1:9944";
   proofServer = "http://127.0.0.1:6300";
   constructor() {
     setNetworkId(NetworkId.Undeployed);
