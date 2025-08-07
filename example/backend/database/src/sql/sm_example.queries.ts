44976223d53889316322a02ef57f267dfacfdbee
Eduardo Soto <esotom92@gmail.com>
2025-08-07 01:59:11 -0400

    undid changes for sm_example.queries

--- example/backend/database/src/sql/sm_example.queries.ts
+++ example/backend/database/src/sql/sm_example.queries.ts
@@ -1,5 +1,5 @@
 /** Types generated for queries found in "src/sql/sm_example.sql" */
-import { PreparedQuery } from "@pgtyped/runtime";
+import { PreparedQuery } from '@pgtyped/runtime';
 
 /** 'InsertStateMachineInput' parameters type */
 export interface IInsertStateMachineInputParams {
@@ -16,36 +16,19 @@ export interface IInsertStateMachineInputQuery {
   result: IInsertStateMachineInputResult;
 }
 
-const insertStateMachineInputIR: any = {
-  "usedParamSet": { "inputs": true, "block_height": true },
-  "params": [{
-    "name": "inputs",
-    "required": true,
-    "transform": { "type": "scalar" },
-    "locs": [{ "a": 65, "b": 72 }],
-  }, {
-    "name": "block_height",
-    "required": true,
-    "transform": { "type": "scalar" },
-    "locs": [{ "a": 75, "b": 88 }],
-  }],
-  "statement":
-    "INSERT INTO user_state_machine \n(inputs, block_height) \nVALUES \n(:inputs!, :block_height!)",
-};
+const insertStateMachineInputIR: any = {"usedParamSet":{"inputs":true,"block_height":true},"params":[{"name":"inputs","required":true,"transform":{"type":"scalar"},"locs":[{"a":65,"b":72}]},{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":75,"b":88}]}],"statement":"INSERT INTO user_state_machine \n(inputs, block_height) \nVALUES \n(:inputs!, :block_height!)"};
 
 /**
  * Query generated from SQL:
  * ```
- * INSERT INTO user_state_machine
- * (inputs, block_height)
- * VALUES
+ * INSERT INTO user_state_machine 
+ * (inputs, block_height) 
+ * VALUES 
  * (:inputs!, :block_height!)
  * ```
  */
-export const insertStateMachineInput = new PreparedQuery<
-  IInsertStateMachineInputParams,
-  IInsertStateMachineInputResult
->(insertStateMachineInputIR);
+export const insertStateMachineInput = new PreparedQuery<IInsertStateMachineInputParams,IInsertStateMachineInputResult>(insertStateMachineInputIR);
+
 
 /** 'GetStateMachineInput' parameters type */
 export type IGetStateMachineInputParams = void;
@@ -63,11 +46,7 @@ export interface IGetStateMachineInputQuery {
   result: IGetStateMachineInputResult;
 }
 
-const getStateMachineInputIR: any = {
-  "usedParamSet": {},
-  "params": [],
-  "statement": "SELECT * FROM user_state_machine \nORDER BY id ASC",
-};
+const getStateMachineInputIR: any = {"usedParamSet":{},"params":[],"statement":"SELECT * FROM user_state_machine"};
 
 /**
  * Query generated from SQL:
@@ -75,10 +54,8 @@ const getStateMachineInputIR: any = {
  * SELECT * FROM user_state_machine
  * ```
  */
-export const getStateMachineInput = new PreparedQuery<
-  IGetStateMachineInputParams,
-  IGetStateMachineInputResult
->(getStateMachineInputIR);
+export const getStateMachineInput = new PreparedQuery<IGetStateMachineInputParams,IGetStateMachineInputResult>(getStateMachineInputIR);
+
 
 /** 'GetStateMachineInputByBlockHeight' parameters type */
 export interface IGetStateMachineInputByBlockHeightParams {
@@ -98,29 +75,17 @@ export interface IGetStateMachineInputByBlockHeightQuery {
   result: IGetStateMachineInputByBlockHeightResult;
 }
 
-const getStateMachineInputByBlockHeightIR: any = {
-  "usedParamSet": { "block_height": true },
-  "params": [{
-    "name": "block_height",
-    "required": true,
-    "transform": { "type": "scalar" },
-    "locs": [{ "a": 55, "b": 68 }],
-  }],
-  "statement":
-    "SELECT * FROM user_state_machine \nWHERE block_height = :block_height!\nORDER BY id ASC",
-};
+const getStateMachineInputByBlockHeightIR: any = {"usedParamSet":{"block_height":true},"params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":55,"b":68}]}],"statement":"SELECT * FROM user_state_machine \nWHERE block_height = :block_height!"};
 
 /**
  * Query generated from SQL:
  * ```
- * SELECT * FROM user_state_machine
+ * SELECT * FROM user_state_machine 
  * WHERE block_height = :block_height!
  * ```
  */
-export const getStateMachineInputByBlockHeight = new PreparedQuery<
-  IGetStateMachineInputByBlockHeightParams,
-  IGetStateMachineInputByBlockHeightResult
->(getStateMachineInputByBlockHeightIR);
+export const getStateMachineInputByBlockHeight = new PreparedQuery<IGetStateMachineInputByBlockHeightParams,IGetStateMachineInputByBlockHeightResult>(getStateMachineInputByBlockHeightIR);
+
 
 /** 'GetLastSumFromExampleTable' parameters type */
 export type IGetLastSumFromExampleTableParams = void;
@@ -136,25 +101,18 @@ export interface IGetLastSumFromExampleTableQuery {
   result: IGetLastSumFromExampleTableResult;
 }
 
-const getLastSumFromExampleTableIR: any = {
-  "usedParamSet": {},
-  "params": [],
-  "statement":
-    "SELECT sum FROM another_example_table \nORDER BY block_height DESC\nLIMIT 1",
-};
+const getLastSumFromExampleTableIR: any = {"usedParamSet":{},"params":[],"statement":"SELECT sum FROM another_example_table \nORDER BY block_height DESC\nLIMIT 1"};
 
 /**
  * Query generated from SQL:
  * ```
- * SELECT sum FROM another_example_table
+ * SELECT sum FROM another_example_table 
  * ORDER BY block_height DESC
  * LIMIT 1
  * ```
  */
-export const getLastSumFromExampleTable = new PreparedQuery<
-  IGetLastSumFromExampleTableParams,
-  IGetLastSumFromExampleTableResult
->(getLastSumFromExampleTableIR);
+export const getLastSumFromExampleTable = new PreparedQuery<IGetLastSumFromExampleTableParams,IGetLastSumFromExampleTableResult>(getLastSumFromExampleTableIR);
+
 
 /** 'InsertSumIntoExampleTable' parameters type */
 export interface IInsertSumIntoExampleTableParams {
@@ -171,33 +129,17 @@ export interface IInsertSumIntoExampleTableQuery {
   result: IInsertSumIntoExampleTableResult;
 }
 
-const insertSumIntoExampleTableIR: any = {
-  "usedParamSet": { "sum": true, "block_height": true },
-  "params": [{
-    "name": "sum",
-    "required": true,
-    "transform": { "type": "scalar" },
-    "locs": [{ "a": 65, "b": 69 }],
-  }, {
-    "name": "block_height",
-    "required": true,
-    "transform": { "type": "scalar" },
-    "locs": [{ "a": 72, "b": 85 }],
-  }],
-  "statement":
-    "INSERT INTO another_example_table \n(sum, block_height) \nVALUES \n(:sum!, :block_height!)",
-};
+const insertSumIntoExampleTableIR: any = {"usedParamSet":{"sum":true,"block_height":true},"params":[{"name":"sum","required":true,"transform":{"type":"scalar"},"locs":[{"a":65,"b":69}]},{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":72,"b":85}]}],"statement":"INSERT INTO another_example_table \n(sum, block_height) \nVALUES \n(:sum!, :block_height!)"};
 
 /**
  * Query generated from SQL:
  * ```
- * INSERT INTO another_example_table
- * (sum, block_height)
- * VALUES
+ * INSERT INTO another_example_table 
+ * (sum, block_height) 
+ * VALUES 
  * (:sum!, :block_height!)
  * ```
  */
-export const insertSumIntoExampleTable = new PreparedQuery<
-  IInsertSumIntoExampleTableParams,
-  IInsertSumIntoExampleTableResult
->(insertSumIntoExampleTableIR);
+export const insertSumIntoExampleTable = new PreparedQuery<IInsertSumIntoExampleTableParams,IInsertSumIntoExampleTableResult>(insertSumIntoExampleTableIR);
+
+
