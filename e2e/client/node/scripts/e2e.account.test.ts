7e7a437c849a7b99773d3b1fbb2801cbdcb3b959
Eddie <edward@dcspark.io>
2025-08-14 11:44:45 -0400

    Removed GITHUB_ACTIONS_SHORT_TEST

--- e2e/client/node/scripts/e2e.account.test.ts
+++ e2e/client/node/scripts/e2e.account.test.ts
@@ -133,9 +133,6 @@ async function assertAccountState(
 
 // Start Test
 export async function accountTests(db: Client, sharedState: SharedState) {
-  if (Deno.env.get("GITHUB_ACTIONS_SHORT_TEST")) {
-    return;
-  }
   const paimaL2 = paimaL2Builder(sharedState);
 
   // wallet-0 is owner
