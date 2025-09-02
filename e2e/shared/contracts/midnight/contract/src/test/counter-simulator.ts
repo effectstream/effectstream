28367e1e9de99d56f5d0f7040c5a29ef3552f7b3
Eduardo Soto <esotom92@gmail.com>
2025-09-02 16:30:42 -0400

    clean up contract files

--- e2e/shared/contracts/midnight/contract/src/test/counter-simulator.ts
+++ e2e/shared/contracts/midnight/contract/src/test/counter-simulator.ts
@@ -1,28 +1,13 @@
-// This file is part of midnightntwrk/example-counter.
-// Copyright (C) 2025 Midnight Foundation
-// SPDX-License-Identifier: Apache-2.0
-// Licensed under the Apache License, Version 2.0 (the "License");
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import {
   type CircuitContext,
+  constructorContext,
   QueryContext,
   sampleContractAddress,
-  constructorContext
 } from "@midnight-ntwrk/compact-runtime";
 import {
   Contract,
   type Ledger,
-  ledger
+  ledger,
 } from "../managed/counter/contract/index.cjs";
 import { type CounterPrivateState, witnesses } from "../witnesses.js";
 
@@ -37,9 +22,9 @@ export class CounterSimulator {
     const {
       currentPrivateState,
       currentContractState,
-      currentZswapLocalState
+      currentZswapLocalState,
     } = this.contract.initialState(
-      constructorContext({ privateCounter: 0 }, "0".repeat(64))
+      constructorContext({ privateCounter: 0 }, "0".repeat(64)),
     );
     this.circuitContext = {
       currentPrivateState,
@@ -47,8 +32,8 @@ export class CounterSimulator {
       originalState: currentContractState,
       transactionContext: new QueryContext(
         currentContractState.data,
-        sampleContractAddress()
-      )
+        sampleContractAddress(),
+      ),
     };
   }
 
@@ -63,7 +48,7 @@ export class CounterSimulator {
   public increment(): Ledger {
     // Update the current context to be the result of executing the circuit.
     this.circuitContext = this.contract.impureCircuits.increment(
-      this.circuitContext
+      this.circuitContext,
     ).context;
     return ledger(this.circuitContext.transactionContext.state);
   }
