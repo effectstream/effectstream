d670177a9b269ef02aab647efe5d7e82f1787554
Eddie <edward@dcspark.io>
2025-09-08 19:46:44 -0300

    userStats

--- templates/chess/packages/client/node/src/api/userStats.ts
+++ templates/chess/packages/client/node/src/api/userStats.ts
@@ -1,45 +1,19 @@
-import { type Static, Type } from "@sinclair/typebox";
 import { getUserRatingPosition, getUserStats } from "@chess/db";
 import type { Pool } from "pg";
-import type fastify from "fastify";
 
-const QuerystringSchema = Type.Object({
-  wallet: Type.String(),
-});
+export const getUserStatsHandler = async (dbConn: Pool, wallet: string) => {
+  wallet = wallet.toLowerCase();
+  const [stats] = await getUserStats.run({ wallet }, dbConn);
 
-const UserStatsSchema = Type.Object({
-  losses: Type.Number(),
-  rating: Type.Number(),
-  ties: Type.Number(),
-  wallet: Type.String(),
-  wins: Type.Number(),
-});
+  if (!stats) {
+    return { stats: null };
+  }
 
-const ResponseSchema = Type.Object({
-  stats: Type.Union([UserStatsSchema, Type.Null()]),
-  rank: Type.Optional(Type.String()),
-});
-
-export function setupUserStats(
-  server: fastify.FastifyInstance,
-  dbConn: Pool,
-): void {
-  server.get<{
-    Querystring: Static<typeof QuerystringSchema>;
-    Reply: Static<typeof ResponseSchema>;
-  }>("/api/user_stats", async (request, reply) => {
-    let { wallet } = request.query;
-    wallet = wallet.toLowerCase();
-    const [stats] = await getUserStats.run({ wallet }, dbConn);
-
-    if (!stats) {
-      reply.send({ stats: null });
-      return;
-    }
-
-    const [ratingPosition] = await getUserRatingPosition.run({
+  const [ratingPosition] = await getUserRatingPosition.run(
+    {
       rating: stats.rating,
-    }, dbConn);
-    reply.send({ stats, rank: ratingPosition?.rank ?? undefined });
-  });
-}
+    },
+    dbConn
+  );
+  return { stats, rank: ratingPosition?.rank ?? undefined };
+};
