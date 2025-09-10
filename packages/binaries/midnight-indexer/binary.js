ed4ee3119c3e54a165f69004b0684bef1f1a7640
Eduardo Soto <esotom92@gmail.com>
2025-09-09 23:42:03 -0300

    fixed indexer url

--- packages/binaries/midnight-indexer/binary.js
+++ packages/binaries/midnight-indexer/binary.js
@@ -39,7 +39,7 @@ function getBinaryUrl() {
   if (!supportedPlatforms.includes(platform)) {
     throw new Error(`Unsupported platform: ${platform}`);
   }
-  return `https://paima-midnight.nyc3.cdn.digitaloceanspaces.com/binaries/indexer-standalone-${platform}.zip`;
+  return `https://paima-midnight.nyc3.cdn.digitaloceanspaces.com/binaries/indexer-standalone-${platform}-${CURRENT_BINARY_VERSION}.zip`;
 }
 
 /*
