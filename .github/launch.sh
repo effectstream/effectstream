78e94d7743501da079ca61e22030aea694f79902
Edward A <ac.edward@gmail.com>
2025-09-11 21:43:08 -0300

    fix cicd

--- .github/launch.sh
+++ .github/launch.sh
@@ -13,9 +13,7 @@ npm --version
 
 # Compile Contracts & Deploy Contracts
 deno task -f @e2e/midnight-contracts midnight-contract:compile
-
 deno task -f @e2e/evm-contracts build:mod
-deno task -f @e2e/evm-contracts deploy:standalone
 
 echo "✅ Contracts compiled & deployed"
 
