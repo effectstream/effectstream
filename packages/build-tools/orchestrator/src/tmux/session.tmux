726bfa29ad02e29b01e0e62e94597f5deadfffec
Tad Hardesty <tad@dcspark.io>
2025-10-02 17:13:56 -0700

    Turn off remain-on-exit and the C-c bind since the children handle it

--- packages/build-tools/orchestrator/src/tmux/session.tmux
+++ packages/build-tools/orchestrator/src/tmux/session.tmux
@@ -1,7 +1,5 @@
 # Our own global configuration.
-set -g remain-on-exit on
 set -g mouse on
-bind -n C-c kill-session
 
 # Spawn our children.
 new-session -d -- deno -A @paima/tui/logs
--- packages/build-tools/orchestrator/src/tmux/session.tmux.ts
+++ packages/build-tools/orchestrator/src/tmux/session.tmux.ts
@@ -1,8 +1,6 @@
 export default `
 # Our own global configuration.
-set -g remain-on-exit on
 set -g mouse on
-bind -n C-c kill-session
 
 # Spawn our children.
 new-session -d -- deno -A @paima/tui/logs
