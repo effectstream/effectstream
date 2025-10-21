50877b7f83d85d04a0caab0de9bfec60946f609c
Eddie <edward@dcspark.io>
2025-10-21 16:31:42 -0300

    Bumped to version 0.3.96 Updated Midnight Fetching System Updated Template MCT

--- packages/node-sdk/sm/primitives/src/midnight-generic/midnight-genetic-grammar.ts
+++ packages/node-sdk/sm/primitives/src/midnight-generic/midnight-genetic-grammar.ts
@@ -3,39 +3,6 @@ import { Type } from "@sinclair/typebox";
 export const midnightGenericGrammar = [
     [
       "payload",
-      Type.Recursive((Self) =>
-        Type.Union([
-          Type.Object({
-            tag: Type.Literal("cell"),
-            content: Self,
-          }),
-          Type.Object({
-            tag: Type.Literal("array"),
-            content: Type.Array(Self),
-          }),
-          Type.Object({
-            tag: Type.Literal('map'),
-            content: Type.Union([
-              Type.Object({}),
-              Type.Array(Type.Tuple([Type.Any(), Type.Any()])),
-            ]),
-          }),
-          Type.Object({
-            value: Type.Array(Type.Record(Type.String(), Type.Number())),
-            alignment: Type.Array(Self),
-          }),
-          Type.Object({
-            tag: Type.Literal("atom"),
-            value: Self,
-          }),
-          Type.Object({
-            tag: Type.Literal("bytes"),
-            length: Type.Number(),
-          }),
-          Type.Object({
-            tag: Type.Optional(Type.Literal("null")),
-          }),
-        ])
-      ),
+      Type.Any()
     ],
   ] as const;
