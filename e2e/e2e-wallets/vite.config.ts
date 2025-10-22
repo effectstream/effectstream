1f9908d901f14b61155f23e194292a9d91bc8de6
Edward A <ac.edward@gmail.com>
2025-10-22 10:22:39 -0300

    Added midnght wasm & contracts to e2e-wallet.

--- e2e/e2e-wallets/vite.config.ts
+++ e2e/e2e-wallets/vite.config.ts
@@ -6,6 +6,7 @@ import "react";
 import "react-dom";
 import { fromFileUrl } from "jsr:@std/path";
 import { join, dirname } from "node:path";
+import wasm from "vite-plugin-wasm";
 
 // This is a workaround to make the workspace imports work.
 const walletPath = join(dirname(fromFileUrl(import.meta.url)), "../../packages/paima-sdk/wallets/");
@@ -14,6 +15,9 @@ const dataTypesPath = join(dirname(fromFileUrl(import.meta.url)), "../shared/dat
 const concisePath = join(dirname(fromFileUrl(import.meta.url)), "../../packages/paima-sdk/concise/");
 const configPath = join(dirname(fromFileUrl(import.meta.url)), "../../packages/paima-sdk/config/");
 const utilsPath = join(dirname(fromFileUrl(import.meta.url)), "../../packages/paima-sdk/utils/");
+
+const midnightContractEip20Path = join(dirname(fromFileUrl(import.meta.url)), "../shared/contracts/midnight/contract-eip-20/src/managed/simpletoken/contract/");
+const midnightContractCounterBasicPath = join(dirname(fromFileUrl(import.meta.url)), "../shared/contracts/midnight/contract-counter/src/managed/counter/contract/");
 
 // This is a mock for @paima/db so it doesn't get loaded in the browser.
 const dbEmptyPath = join(dirname(fromFileUrl(import.meta.url)), "paima-db-empty.ts");
@@ -24,6 +28,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      "@e2e/midnight-contract-eip-20/contract": midnightContractEip20Path + "index.ts",
+      "@e2e/midnight-contract-counter-basic/contract": midnightContractCounterBasicPath + "index.ts",
       "@paima/utils": utilsPath + "src/mod.ts",
       "@paima/config": configPath + "src/mod.ts",
       "@paima/concise": concisePath + "src/mod.ts",
@@ -44,12 +50,14 @@ export default defineConfig({
     open: true,
   },
   plugins: [
+    wasm(),
     react(),
     deno(),
     nodePolyfills(),
   ],
 
   optimizeDeps: {
+    exclude: ["@midnight-ntwrk/onchain-runtime"],
     esbuildOptions: {
       target: "esnext",
     },
