f65dd846abcb8d92de5baa0491393f10d676c5ef
Eddie <edward@dcspark.io>
2026-05-14 21:55:32 -0400

    Remove legacy chess and evm-midnight templates
    These templates still used the old @paimaexample namespace and have
    been superseded by chess-v2 and evm-midnight-v2 which use @effectstream.

--- templates/chess/packages/shared/contracts/evm/ignition/modules/oz-erc20dev.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/ignition-core";
-
-export default buildModule("OpenZeppelinErc20DevModule", (m) => {
-  const contract = m.contract("MyOpenZeppelinErc20Dev", []);
-  return { contract };
-});
--- templates/evm-midnight/packages/shared/contracts/evm/ignition/modules/erc20dev.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/ignition-core";
-
-export default buildModule("PaimaErc20DevModule", (m) => {
-  const contract = m.contract("PaimaErc20Dev", []);
-  return { contract };
-});
